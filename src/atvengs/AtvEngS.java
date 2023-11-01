9539227284d13c73784afe096e58709859c8ea14
unknown <ra183068@ACAD.UCDB.BR>
2023-11-01 19:48:58 -0400

    mensagem2

---
+++ src/atvengs/AtvEngS.java
@@ -0,0 +1,10 @@
+package atvengs;
+public class AtvEngS {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+    
+}
